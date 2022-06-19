38cc64605927ac1e3b5c422b42c735ac64549c65
sanjay gorai <spgorai@gmail.com>
2022-06-19 17:24:15 +0000

    modified code added

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -1,9 +1,13 @@
+import java.sql.SQLOutput;
+
 public class LoginPage {
 
     public static void main(String a[])
     {
         System.out.println("hello LoginPage");
         System.out.println("Added line");
+        System.out.println("Login into Website");
+        System.out.println("Log out from website");
     }
 
 }
--- src/main/java/SearchPage.java
+++ src/main/java/SearchPage.java
@@ -3,6 +3,7 @@ public class SearchPage {
     public static void main(String a[])
     {
         System.out.println("hello SearchPage");
+        System.out.println("Search some words");
     }
 
 }
